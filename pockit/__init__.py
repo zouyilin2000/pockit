591493d0b148f7f4d5e436bc48174bc9e8fda736
ZouYilin <safe.dust4704@fastmail.com>
2024-11-30 00:42:24 +0800

    Update documentation in __init__.py with project and documentation links

--- pockit/__init__.py
+++ pockit/__init__.py
@@ -8,6 +8,8 @@
 - 💪 **Powerful:** Pockit can handle basically any optimal problem. It employs a multiple-phase optimal control model that allows continuous/non-continuous state and control variables, path/integral/boundary constraints, and fixed/free initial and terminal time.
 - 🔢 **Easy to use:** Pockit is designed to be easy to use. It provides a [SymPy](https://www.sympy.org/)-based, intuitive interface for defining and solving problems.
 - ⚡ **Fast:** Pockit is fast. It uses various techniques to speed up the entire compilation and solution process, including symbolic differentiation (with [SymPy](https://www.sympy.org/)), Just-In-Time compilation (with [Numba](https://numba.pydata.org/)), vectorization, and more.
+
+Please visit the [project page](https://github.com/zouyilin2000/pockit) and the [documentation](https://pockit-docs.notion.site) for more information.
 """
 
 __author__ = "Yilin Zou"
